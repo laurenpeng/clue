a0d62697da9d9c006c2b64530a8672a5fb7e08a8
jhomble <jhomble7@gmail.com>
2019-10-16 00:19:47 -0400

    deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,21 +5,6 @@ import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  {
-    path: 'login',
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
-  },
-  {
-    path: 'kanban',
-    loadChildren: () =>
-      import('./kanban/kanban.module').then(m => m.KanbanModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'customers',
-    loadChildren: () =>
-      import('./customers/customers.module').then(m => m.CustomersModule),
-  }
 ];
 
 @NgModule({
